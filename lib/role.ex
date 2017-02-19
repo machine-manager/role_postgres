f9d4b79c7bb4e0f280ba5883ece838cba28bc52f
Ivan Kozik <ivan@ludios.org>
2017-02-19 13:59:02 +0000

    role_postgres

---
+++ lib/role.ex
@@ -0,0 +1,15 @@
+defmodule RolePostgres do
+	def role(_tags \\ []) do
+		%{
+			desired_packages:  ["postgresql"],
+			ssh_allow_users:   ["postgres"],
+			ferm_output_chain:
+				"""
+				outerface lo {
+					# PostgreSQL does stats collection on a random UDP port
+					proto udp daddr 127.0.0.1 mod owner uid-owner postgres ACCEPT;
+				}
+				"""
+		}
+	end
+end
